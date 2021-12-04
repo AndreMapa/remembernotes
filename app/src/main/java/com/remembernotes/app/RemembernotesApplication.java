f4c8a779b0d5d964da0a056ca99143cb3a1d5adb
Mickael Luiz Machado <mickaelluiz843@gmail.com>
2021-12-03 21:02:41 -0300

    upload project

---
+++ app/src/main/java/com/remembernotes/app/RemembernotesApplication.java
@@ -0,0 +1,13 @@
+package com.remembernotes.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemembernotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemembernotesApplication.class, args);
+	}
+
+}
